202cd81f4cfddd60d7635484e691b7eba75ae2c3
DM <dsbmach@gmail.com>
2020-12-27 14:53:11 -0500

    add current weather button

---
+++ src/components/WeatherBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const WeatherBtn = () => {
+    return (
+        <Button color="primary" variant="contained">
+            Load Weather
+        </Button>
+    );
+};
+
+export default WeatherBtn;
